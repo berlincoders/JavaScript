61a837dc0dfffba8a92b0c96f46aad12ae24f631
Romeo Asinari <ralonsoad@gmail.com>
2023-06-22 11:53:46 +0200

    Conditions2

---
+++ script.js
@@ -0,0 +1,38 @@
+// you can run the code with node, from the terminal typing:
+//  node script.js 
+// please check that you are in the correct directory otherwiswe
+// you will have an error like this one:
+//
+// node:internal/modules/cjs/loader:1078
+// throw err;
+//
+// this is just a simple example to display the data type
+console.log (typeof(42));
+
+// create an Object very important the coma
+const musician = {
+    firstname:"john",
+    lastname:"lennon"
+};
+
+//Read and change an Object
+// Read
+
+//change
+musician.firstname="Ringo"
+console.log(musician.firstname);
+
+// Variable let or const
+let sum=0;
+// sum can be re- assigned
+const firstname= "john";
+// firstname can not be re-assigned
+
+// lets concantenate two strings
+
+const firstString="Hello";
+const secondString=" world";
+
+const bouthStrigns= firstString +secondString;
+console.log(bouthStrigns);
+
